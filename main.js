b607efced8fd1e1209330f1dbc307ba679922fab
alowtron <alowtron@gmail.com>
2025-08-19 16:45:47 -0400

    now It randomly make tons of random cars

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ const mainCtx = mainCanvas.getContext("2d")
 const networkCtx = networkCanvas.getContext("2d")
 
 const road = new Road(mainCanvas.width / 2, mainCanvas.width * 0.95)
-const car = new Car(road.getLaneCenter(4), 100, 30, 50, "AI", 4)
+// const car = new Car(road.getLaneCenter(4), 100, 30, 50, "AI", 4)
+const numberOfCars = 100
+const cars = generateCars(numberOfCars)
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 1.5)
 ]
@@ -18,26 +20,40 @@ const traffic = [
 
 animate()
 
+function generateCars(numberOfCars) {
+  const cars = []
+  for (let i = 0; i < numberOfCars; i++) {
+    cars.push(new Car(road.getLaneCenter(4), 100, 30, 50, "AI", 4))
+  }
+  return cars
+}
+
 function animate() {
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, [])
   }
-  car.update(road.borders, traffic)
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].update(road.borders, traffic)
+  }
+  // car.update(road.borders, traffic)
   mainCanvas.height = window.innerHeight
   networkCanvas.height = window.innerHeight
 
 
   mainCtx.save()
-  mainCtx.translate(0, -car.y + mainCanvas.height * 0.75)
+  mainCtx.translate(0, -cars[0].y + mainCanvas.height * 0.75)
 
   road.draw(mainCtx)
   for (let i = 0; i < traffic.length; i++) {
     traffic[i].draw(mainCtx, "blue")
   }
-  car.draw(mainCtx, "yellow")
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(mainCtx, "yellow")
+  }
+  // car.draw(mainCtx, "yellow")
 
   mainCtx.restore()
 
-  Visualizer.drawNetwork(networkCtx, car.brain)
+  Visualizer.drawNetwork(networkCtx, cars[0].brain)
   requestAnimationFrame(animate)
 }
